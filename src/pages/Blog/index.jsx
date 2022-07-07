ce8d404fcadc60d4b7b850555c6e2c1bd66a3a63
FedeRBeije <federico.reghini@beije.it>
2022-07-07 16:04:24 +0200

    impostato base logica blog

--- src/pages/Blog/index.jsx
+++ src/pages/Blog/index.jsx
@@ -1,83 +1,91 @@
 import { useState, useEffect } from "react";
-import { Link, useParams } from "react-router-dom";
-import { format } from "date-fns";
-import useService from "../../hooks/useService";
-import { toast, ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
 
+// router & format
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { format } from "date-fns";
+
+// hooks
+import useService from "../../hooks/useService";
+
+// components
 import Input from "../../components/Input";
 import Checkbox from "../../components/Checkbox";
-import Select from "../../components/Select";
 import MDEditor from "../../components/MDEditor";
 
+// styles
 import styles from "./styles.module.css";
-import { useId } from "react";
 
 const emptyState = {
-  title_it: "",
-  description_it: "",
-  title_en: "-",
-  description_en: "-",
-  type: "",
-  mode: "",
-  date_creation: format(Date.now(), "yyyy-MM-dd"),
-  academy: false,
-  disable_date: null,
+  title: "",
+  subtitle: "",
+  language: "",
+  description: "",
+  images: [],
+  author: "",
+  createDateTime: format(Date.now(), "yyyy-MM-dd"),
+  cover_img: "",
   permalink: "",
 };
 
 
 const Blog = ({ isNew }) => {
+
   const { id } = useParams();
-  const toastId = useId();
+
   const [state, setState] = useState(emptyState);
 
-  const notify = (type, message) => {
-    toast[type](message, { 
-      position: toast.POSITION.TOP_CENTER,
-      toastId
-    })
-  }
+  const navigate = useNavigate();
 
-  const [getJobResult, getJob] = useService(`/job_application/${id}`);
+  // api
+  const [getBlogResult, getBlog] = useService(`/blog/id/${id}`);
 
-  const [saveJobResult, saveJob] = useService("/job_application", {
+  const [saveBlogResult, saveBlog] = useService("/blog", {
     method: "post",
   });
 
-  const [updateJobResult, updateJob] = useService(`/job_application/${id}`, {
+  const [updateBlogResult, updateBlog] = useService(`/blog/id/${id}`, {
     method: "put",
   });
 
   useEffect(() => {
-    if (!isNew) getJob()
+    if (!isNew) getBlog()
   }, []);
-  
+
   useEffect(() => {
-    const { response } = getJobResult ?? { response: null };
+    const { response } = getBlogResult ?? { response: null };
     if (response) {
       setState(response);
     }
-  }, [getJobResult?.response]);
+  }, [getBlogResult?.response]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (isNew) {
+      await saveBlog({ ...state, createDateTime: new Date(state.createDateTime).getTime() });
+      navigate('/blogs', {
+        state: {
+          toast: saveBlogResult.error === null ? true : false
+        }
+      })
+    } else {
+      await updateBlog({ ...state });
+      navigate('/blogs', {
+        state: {
+          toast: updateBlogResult.response ? true : false
+        }
+      })
+    }
+  }
 
   return (
     <div className={styles["container"]}>
       <form
-        onSubmit={(e) => {
-          e.preventDefault();
-
-          if (isNew) saveJob(state).then(res => {
-            if (res) notify('success', "Salvato!");
-          }).catch(err => notify("error", `Qualcosa è andato storto: ${err?.message}`));
-
-          else updateJob(state).then(res => {
-            if (res) notify("success", "Aggiornato!");
-          }).catch(err => notify("error", `Qualcosa è andato storto: ${err?.message}`));
-        }}
+        onSubmit={handleSubmit}
       >
         <div className={styles["title-row"]}>
           <Link
-            to="/jobs"
+            to="/blogs"
             style={{
               fontSize: "200%",
               textDecoration: "none",
@@ -88,72 +96,58 @@ const Blog = ({ isNew }) => {
           </Link>
           <h2>
             {isNew
-              ? "Nuova offerta di lavoro"
-              : getJobResult.response
-                ? `Modifica ${getJobResult.response.title_it}`
+              ? "Nuovo post"
+              : getBlogResult.response
+                ? `Modifica ${getBlogResult.response.title}`
                 : ""}
           </h2>
           <button type="submit" className="primary-button">
             Salva
           </button>
         </div>
-        {(isNew || getJobResult.response) && (
+        {(isNew || getBlogResult.response) && (
           <>
             <div className={styles["inputs-row"]}>
               <Input
                 style={{ width: "40%" }}
                 placeholder="Titolo"
                 name="title"
-                value={state.title_it}
+                value={state.title}
                 onChange={(e) =>
-                  setState((p) => ({ ...p, title_it: e.target.value }))
+                  setState((p) => ({ ...p, title: e.target.value }))
                 }
-              />
-              <Input
-                placeholder="Posizione"
-                name="type"
-                value={state.type}
-                onChange={(e) =>
-                  setState((p) => ({ ...p, type: e.target.value }))
-                }
-              />
-              <Select
-                value={state.mode}
-                label="Sede"
-                options={[
-                  { value: "-", label: "vuoto" },
-                  { value: "remote", label: "Da remoto" },
-                  { value: "milan", label: "Milano" },
-                  { value: "hybrid", label: "Ibrido" },
-                ]}
-                onChange={(mode) => setState((p) => ({ ...p, mode }))}
-              />
-              <Checkbox
-                checked={state.academy}
-                onChange={(e) => {
-                  setState((p) => ({ ...p, academy: e.target.checked }));
-                }}
-                label="Academy: "
               />
 
-              <Checkbox
-                checked={state.disable_date}
-                onChange={(e) => {
-                  setState((p) => ({ ...p, disable_date: e.target.checked }));
-                }}
-                label="Visibile: "
+              <Input
+                style={{ width: "40%" }}
+                placeholder="Autore"
+                name="title"
+                value={state.author}
+                onChange={(e) =>
+                  setState((p) => ({ ...p, author: e.target.value }))
+                }
               />
+
+              {
+                !isNew &&
+                <Checkbox
+                  checked={state.disable_date}
+                  onChange={(e) => {
+                    setState((p) => ({ ...p, disable_date: e.target.checked }));
+                  }}
+                  label="Visibile: "
+                />
+              }
             </div>
             <MDEditor
-              value={state.description_it}
+              value={state.description}
               onChange={(e) =>
-                setState((p) => ({ ...p, description_it: e.target.value }))
+                setState((p) => ({ ...p, description: e.target.value }))
               }
             />
           </>
         )}
       </form>
-      <ToastContainer />
     </div>
   );
 };
--- src/pages/Blogs/index.jsx
+++ src/pages/Blogs/index.jsx
@@ -1,92 +1,75 @@
-import { Link, useNavigate } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useId } from "react";
+
+// router
+import { Link, useLocation, useNavigate } from "react-router-dom";
+
+// hooks & components
 import useService from "../../hooks/useService";
 import Table from "../../components/Table";
-
-import styles from "./styles.module.css";
+import { notify, ToastContainer } from "../../utils/toast";
 import Loader from "../../components/Loader";
-import Select from "../../components/Select";
-import { useState } from "react";
 
-const initState = {
-  academy: "all",
-  active: "all",
-}
+// style
+import styles from "./styles.module.css";
 
 const Blogs = () => {
 
-  const [state, setState] = useState(initState);
-
-  const [{ response, error, loading }, getBlogs] =
+  const [{ response }, getBlogs] =
     useService("/blogs");
 
+  const toastId = useId();
+
   const navigate = useNavigate();
+  const location = useLocation();
 
   useEffect(() => {
     getBlogs();
   }, []);
 
+  useEffect(() => {
+    if(location.state !== null){
+      location.state?.toast === true ? notify("success", toastId) : notify("error", toastId)
+    }
+  }, []);
+
   return (
     response ?
       <div className={styles["container"]}>
-        {console.log(response)}
         <div className={styles["wrapper"]}>
           <div className={styles["header"]}>
 
             <h1>Posts</h1>
 
-            {/* <Select
-              value={state.active} //aggiungere stato per il valore
-              label="Attivi"
-              options={[
-                { value: "all", label: "Tutti" },
-                { value: "yes", label: "Attivi" },
-                { value: "no", label: "Non attivi" },
-              ]}
-              onChange={(active) => setState((p) => ({ ...p, active }))}
-            />
-
-            <Select
-              value={state.academy} //aggiungere stato per il valore
-              label="Academy"
-              options={[
-                { value: "all", label: "Tutti" },
-                { value: "yes", label: "Academy" },
-                { value: "no", label: "Non Academy" },
-              ]}
-              onChange={(academy) => setState((p) => ({ ...p, academy }))}
-            /> */}
             <Link to="new" className="primary-button">
               + Nuovo Post
             </Link>
           </div>
-          {response && (
             <Table
               headers={[
                 "ID",
                 "Titolo",
                 "Autore",
-                "Data di creazione",
+                // "Data di creazione",
               ]}
               records={response.map(
                 ({
                   id,
                   title,
                   author,
-                  createDateTime,
+                  // createDateTime,
                 }) => ({
                   id,
                   title,
                   author,
-                  createDateTime: `${createDateTime.dayOfMonth}/${createDateTime.monthValue}/${createDateTime.year}`,
+                  // createDateTime: `${createDateTime.dayOfMonth} ${createDateTime.month.toLowerCase()} ${createDateTime.year}`,
                 })
               )}
               actionLabel="Modifica"
               onAction={(record) => navigate(record.id.toString())}
               formatDimension={250}
             />
-          )}
         </div>
+        <ToastContainer />
       </div>
       :
       <Loader />
