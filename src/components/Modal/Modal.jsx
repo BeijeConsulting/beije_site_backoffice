bfaf580d04380d757739252b6569b98dc81ccdca
FedeRBeije <federico.reghini@beije.it>
2022-07-08 14:27:25 +0200

    componente modal creato

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,9 +1,14 @@
-const Modal = ({ shouldShow, onRequestClose, children }) => {
+import  './styles.css';
+const Modal = ({ shouldShow, onRequestClose, onRequestYes, children }) => {
 
 	return shouldShow ? (
 		<div className="modalBackground" onClick={onRequestClose}>
 			<div className="modalBody" onClick={e => e.stopPropagation()}>
 				{children}
+       <div className='btnContainer'>
+         <button className="secondary-button btnMargin" onClick={onRequestClose}>No</button>
+         <button className="primary-button" onClick={onRequestYes}>Si</button>
+       </div>
 			</div>
 		</div>
 	) : null;
