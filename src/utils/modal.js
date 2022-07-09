cacdc8ec2712a0c4a8301abdf133f8ae3f776b73
FedeRBeije <federico.reghini@beije.it>
2022-07-09 12:04:16 +0200

    creato funzione generica per la modale

---
+++ src/utils/modal.js
@@ -0,0 +1,12 @@
+export const handleRequestsModal = (type, onYes,  setStateForModal) => () => {
+  switch (type) {
+    case "yes":
+      onYes();
+      setStateForModal(false);
+      break;
+
+    default:
+      setStateForModal(false);
+      break;
+  }
+}
