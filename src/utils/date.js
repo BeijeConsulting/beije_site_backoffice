63c40e7c8969830533ca9913160b2033e774d765
FedeRBeije <federico.reghini@beije.it>
2022-07-11 15:59:20 +0200

    bug fixed in date,js

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,5 +1,5 @@
 const date = new Date();
 
 export function todayWithTime() {
-  return `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}T${date.getHours()}:${date.getMinutes()}`
+  return `${date.getFullYear()}-${date.getMonth() < 10 ? "0" + date.getMonth(): date.getMonth()}-${date.getDate() < 10 ? "0" + date.getDate() : date.getDate()}T${date.getHours()}:${date.getMinutes()}`
 }
