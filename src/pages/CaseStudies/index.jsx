97f9f01068f74f5a6a8b3bbdbf0dc74af2062911
FedeRBeije <federico.reghini@beije.it>
2022-07-12 17:02:48 +0200

    creato componenti CaseStudies e CaseStudy

--- src/pages/CaseStudies/index.jsx
+++ src/pages/CaseStudies/index.jsx
@@ -1,11 +1,98 @@
-import React from 'react'
+import React, { useEffect, useState, useId } from 'react';
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { format } from 'date-fns/esm';
+import locale from "date-fns/locale/it";
+
+// hooks
+import useService from '../../hooks/useService';
+import { notify , ToastContainer} from '../../utils/toast';
+
+// components
+import Loader from '../../components/Loader';
+import Select from '../../components/Select';
+import Table from '../../components/Table';
+
+// styles
+import styles from "./styles.module.css";
+
+
+const initState = {
+  lang: "it",
+}
 
 function CaseStudies() {
+  const [state, setState] = useState(initState);
+  
+  const [{ response }, getCaseStudies] =
+    useService(`/casestudies/${state.lang}`);
+
+  const toastId = useId();
+
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    getCaseStudies();
+    if (location.state !== null) {
+      notify("success", toastId);
+    }
+  }, [state.lang]);
+
+
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    response ?
+      <div className={styles["container"]}>
+        <div className={styles["wrapper"]}>
+          <div className={styles["header"]}>
+
+            <h1>Case Studies</h1>
+
+            <Select
+              value={state.lang}
+              label="Lingua"
+              options={[
+                { value: "it", label: "Italiano" },
+                { value: "en", label: "Inglese" },
+              ]}
+              onChange={(lang) => {
+                setState((p) => ({ ...p, lang }))}}
+            />
+
+            <Link to="new" className="primary-button">
+              + Nuovo Case Study
+            </Link>
+          </div>
+          <Table
+            headers={[
+              "ID",
+              "Titolo",
+              "Sottotitolo",
+              // "Descrizione",
+            ]}
+            records={response.map(
+              ({
+                id,
+                title,
+                subtitle,
+                // description,
+              }) => ({
+                id,
+                title,
+                subtitle,
+                // description,
+              })
+            )}
+            actionLabel="Modifica"
+            onAction={(record) => navigate(record.id.toString())}
+            formatDimension={250}
+          />
+        </div>
+        <ToastContainer />
+      </div>
+      :
+      <Loader />
+  );
+};
 
 export default CaseStudies
