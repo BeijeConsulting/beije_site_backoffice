ae66b85acdafd6d54c411499ef4a034f854ec815
FedeRBeije <federico.reghini@beije.it>
2022-07-12 15:10:28 +0200

    tolto -eng sul translate permalink

--- src/pages/Blog/index.jsx
+++ src/pages/Blog/index.jsx
@@ -102,7 +102,7 @@ const Blog = ({ isNew }) => {
         create_datetime: isNew ? todayWithTime() : format(state.create_datetime, "yyyy-MM-dd'T'HH:mm"),
         permalink: state.permalink,
         cover_img: null,
-        translate_blog_permalink: isNew ? state.permalink + "-eng" : state.translate_blog_permalink
+        translate_blog_permalink: isNew ? state.permalink : state.translate_blog_permalink
       });
   }
 
