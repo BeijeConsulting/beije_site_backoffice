33fb192795dc1d1534b0f3f72c37c27c7ed6d9ff
FedeRBeije <federico.reghini@beije.it>
2022-07-19 12:39:02 +0200

    creato Componente per i bottoni save

---
+++ src/components/SaveContainer/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from "./styles.module.css";
+
+function SaveContainer({ isNew, onSubmit }) {
+
+  return (
+    <div className={styles["save-container"]}>
+      <button type="submit" className="success-button"
+        onClick={onSubmit}>
+        Salvataggio rapido
+      </button>
+
+      <button name="quickSave" type="submit" className="success-button"
+        onClick={onSubmit}>
+        {isNew ? "Salva" : "Salva modifiche"}
+      </button>
+    </div>
+  )
+}
+
+const SaveContainerMemo = React.memo(SaveContainer);
+
+export default SaveContainerMemo;
