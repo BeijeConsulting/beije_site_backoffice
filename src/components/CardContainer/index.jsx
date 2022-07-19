a6e2cff7546844cc44f283713433bbc1d727d814
FedeRBeije <federico.reghini@beije.it>
2022-07-19 15:33:27 +0200

    Aggiunto style prop

--- src/components/CardContainer/index.jsx
+++ src/components/CardContainer/index.jsx
@@ -1,9 +1,9 @@
 import { memo } from 'react';
 import styles from './styles.module.css';
 
-function CardContainer({ children, head }) {
+function CardContainer({ children, head, style }) {
   return (
-    <div className={styles["card"]}>
+    <div className={styles["card"]} style={{...style}}>
       <h4 className={styles["title"]}>{head}</h4>
       {children}
     </div>
